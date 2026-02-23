083e1c8f245f945de75b462fe5fa6942affeff50
Tushar Suara <tsb020@nist.edu>
2026-02-23 17:02:37 +0530

    Resolve stash conflict in text.py

--- text.py
+++ text.py
@@ -1,8 +1,16 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 # dev A new line... (UPDATED BY A)
 =======
 # dev  new line...
 >>>>>>> 66fce86 (DevB changes same line crash)
+=======
+<<<<<<< Updated upstream
+# dev A new line...
+=======
+# dev A new line... (UPDATED BY A)huhu
+>>>>>>> Stashed changes
+>>>>>>> 0e3a8a3 (Resolve stash conflict in text.py)
 # dev Another new line...
 #new line for dev B
 # dev And another new line...
